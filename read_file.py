a059cf407bdfc9cb74f52fa2f509ee22b64bb80c
Cezar Todirisca <cezartodirisca@gmail.com>
2023-12-26 20:11:35 +0200

    Same
    Co-Authored-By: Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>

---
+++ read_file.py
@@ -0,0 +1,34 @@
+
+def read_info(file):
+    with open(file, 'r') as f:
+        first_line = f.readline()
+        second_line = f.readline().split()
+
+        n_tasks = int(first_line.split()[0])
+        res_not = int(first_line.split()[1])
+        res_yes = int(first_line.split()[2])
+
+        res_not_max = [int(i) for i in second_line[:res_not]]
+        res_yes_max = [int(i) for i in second_line[res_not:]]
+
+        resources = [res_not_max, res_yes_max]
+
+        durations = []
+        res_needed = []
+        res_consumption = []
+        res_produced = []
+
+        n_successors = []
+        successors = []
+
+        for line in f:
+            line = line.split()
+            durations.append(int(line[0]))
+            res_needed.append([int(i) for i in line[1:res_not+1]])
+            res_consumption.append([int(i) for i in line[res_not+1:res_not+res_yes+1]])
+            res_produced.append([int(i) for i in line[res_not+res_yes+1:res_not+2*res_yes+1]])
+
+            n_successors.append(int(line[res_not+2*res_yes+1]))
+            successors.append([int(i) for i in line[-n_successors[-1]:]] if n_successors[-1] > 0 else [])
+
+        return n_tasks, resources, durations, res_needed, res_consumption, res_produced, n_successors, successors
---
+++ test_dt.py
@@ -0,0 +1,88 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+from read_file import read_info
+
+
+def get_predecessors(index, succesors):
+    predecessors = []
+    for i in range(len(succesors)):
+        if index+1 in succesors[i]:
+            predecessors.append(i)
+    return predecessors
+
+
+# Create a new model
+model = gp.Model("mip1")
+
+# use a branch and bound algorithm
+model.setParam('Method', 2)
+
+n_tasks, resources, durations, res_needed, res_consumption, res_produced, n_successors, successors = read_info('ConsProd_j07.rcp')
+predecessors = [get_predecessors(i, successors) for i in range(n_tasks)]
+t_max = sum(durations)
+
+x = model.addMVar((n_tasks, t_max+1), vtype=GRB.BINARY, name="x") # x[i,t] = 1 if task i is executed at time t
+# Set other data
+earliest = 0
+latest = t_max
+
+t = [i for i in range(earliest, latest+1)]
+
+# Set objective
+model.setObjective(gp.quicksum(t*x[i,t] for i in range(n_tasks) for t in range(earliest, latest+1)), GRB.MINIMIZE)
+
+# get all precedences
+for i in range(n_tasks):
+    pred_i = predecessors[i]
+    ES_i = sum(durations[j] for j in pred_i) + 1 if len(pred_i) > 0 else 0
+    LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
+    
+    for j in successors[i]:
+        ES_j = sum(durations[k] for k in predecessors[j-1]) + 1 if len(predecessors[j-1]) > 0 else 0
+        LS_j = latest - sum(durations[k-1] for k in successors[j-1]) + 1
+    
+        sum_left = gp.quicksum(t*x[j-1, t] for t in range(ES_j, LS_j))
+        sum_right = gp.quicksum(t*x[i-1, t] for t in range(ES_i, LS_i)) + durations[i-1]
+        model.addConstr(sum_left >= sum_right)
+
+for t in range(earliest, latest+1):
+    for r in range(len(resources[0])):
+        for i in range(n_tasks):
+            ES_i = sum(durations[j] for j in predecessors[i]) + 1 if len(predecessors[i]) > 0 else 0
+            LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
+            inner_sum = 0
+            for tau in range(max(ES_i, t-durations[i]+1), min(LS_i, t)):
+                inner_sum += x[i, tau]
+
+        sum_left = gp.quicksum(res_needed[i][r]*inner_sum for i in range(n_tasks))
+        model.addConstr(sum_left <= resources[0][r])
+
+for i in range(n_tasks):
+    ES_i = sum(durations[j] for j in predecessors[i]) + 1 if len(predecessors[i]) > 0 else 0
+    LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
+
+    left_sum = gp.quicksum(x[i, t] for t in range(ES_i, LS_i))
+    model.addConstr(left_sum == 1)
+
+P = 3
+s = [[0 for _ in range(P)] for _ in range(t+1)]
+for p in range(P):
+    inner_sum = [x[i, 0] * res_consumption[i][p] for i in range(n_tasks)]
+    s[0][p] = resources[1][p] - sum(inner_sum)
+
+for t in range(1, latest+1):
+    for p in range(P):
+        first_sum = [x[i, t-durations[i]] * res_produced[i][p] for i in range(n_tasks)]
+        second_lum = [x[i, t] * res_consumption[i][p] for i in range(n_tasks)]
+        s[t][p] = s[t-1][p] + sum(first_sum) - sum(second_lum)
+
+for t in range(earliest, latest+1):
+    for p in range(P):
+        model.addConstr(s[t][p] >= 0)
+        
+model.optimize()
+
+temp = model.x
+print(len(temp))
+print(model.objVal)
