e65d7442c5fdf29665ed0f96bc02ba8b6a6242f5
Cezar Todirisca <cezartodirisca@gmail.com>
2023-12-27 11:59:58 +0200

    fix dt & ddt
    Co-Authored-By: Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>

--- test_ddt.py
+++ test_ddt.py
@@ -7,9 +7,11 @@ from read_file import read_info
 def get_predecessors(index, succesors):
     predecessors = []
     for i in range(len(succesors)):
-        if index+1 in succesors[i]:
+        if index in succesors[i]:
             predecessors.append(i)
-    return predecessors
+            predecessors += get_predecessors(i, succesors)
+
+    return set(predecessors)
 
 
 # Create a new model
@@ -19,7 +21,7 @@ model = gp.Model("mip1")
 model.setParam('Method', 2)
 
 n_tasks, resources, durations, res_needed, res_consumption, res_produced, n_successors, successors = read_info('ConsProd_j07.rcp')
-predecessors = [get_predecessors(i, successors) for i in range(n_tasks)]
+predecessors = [get_predecessors(i, successors) for i in range(1, n_tasks+1)]
 t_max = sum(durations)
 
 x = model.addMVar((n_tasks, t_max+1), vtype=GRB.BINARY, name="x") # x[i,t] = 1 if task i is executed at time t
@@ -27,15 +29,17 @@ x = model.addMVar((n_tasks, t_max+1), vtype=GRB.BINARY, name="x") # x[i,t] = 1 i
 earliest = 0
 latest = t_max
 
-t = [i for i in range(earliest, latest+1)]
 
 # Set objective
 model.setObjective(gp.quicksum(t*x[i,t] for i in range(n_tasks) for t in range(earliest, latest+1)), GRB.MINIMIZE)
 
+# indici din fisier -> i - 1
+# indici doar din cod -> i
+
 # get all precedences
 for i in range(n_tasks):
     pred_i = predecessors[i]
-    ES_i = sum(durations[j] for j in pred_i) + 1 if len(pred_i) > 0 else 0
+    ES_i = sum(durations[j] for j in predecessors[i]) + 1 if len(predecessors[i]) > 0 else 0
     LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
     
     for j in successors[i]:
@@ -43,7 +47,7 @@ for i in range(n_tasks):
         LS_j = latest - sum(durations[k-1] for k in successors[j-1]) + 1
     
         sum_left = gp.quicksum(t*x[j-1, t] for t in range(ES_j, LS_j))
-        sum_right = gp.quicksum(t*x[i-1, t] for t in range(ES_i, LS_i)) + durations[i-1]
+        sum_right = gp.quicksum(t*x[i, t] for t in range(ES_i, LS_i)) + durations[i]
         model.addConstr(sum_left >= sum_right)
 
 for t in range(earliest, latest+1):
@@ -51,6 +55,7 @@ for t in range(earliest, latest+1):
         for i in range(n_tasks):
             ES_i = sum(durations[j] for j in predecessors[i]) + 1 if len(predecessors[i]) > 0 else 0
             LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
+
             inner_sum = 0
             for tau in range(max(ES_i, t-durations[i]+1), min(LS_i, t)):
                 inner_sum += x[i, tau]
@@ -66,6 +71,7 @@ for i in range(n_tasks):
 
 P = 3
 s = [[0 for _ in range(P)] for _ in range(t+1)]
+
 for p in range(P):
     inner_sum = [x[i, 0] * res_consumption[i][p] for i in range(n_tasks)]
     s[0][p] = resources[1][p] - sum(inner_sum)
@@ -79,16 +85,19 @@ for t in range(1, latest+1):
 for t in range(earliest, latest+1):
     for p in range(P):
         model.addConstr(s[t][p] >= 0)
-
+      
 for i in range(n_tasks):
+   
     ES_i = sum(durations[j] for j in predecessors[i]) + 1 if len(predecessors[i]) > 0 else 0
     LS_i = latest - sum(durations[j-1] for j in successors[i]) + 1
+
     for t in range(ES_i, LS_i+1):
         first_sum = gp.quicksum(x[i, tau] for tau in range(t, LS_i))
         
         for j in successors[i]:
             LS_j = latest - sum(durations[k-1] for k in successors[j-1]) + 1
             ES_j = sum(durations[k] for k in predecessors[j-1]) + 1 if len(predecessors[j-1]) > 0 else 0
+
             second_sum = gp.quicksum(x[j-1, tau] for tau in range(ES_j, min(LS_j, t+durations[i]-1)))
 
             model.addConstr(first_sum + second_sum <= 1)
